9abdb224a110b7c02e706432fdc96bf08d400bee
geekyChanakya <your-email@bangthetable.com>
2020-04-03 13:06:22 +0530

    Updates

---
+++ precache-manifest.b918df10c58b52d9add070fac8f33714.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ae747d407246a81c9f8c6bd949bdbc2",
+    "url": "/covid-tracker/index.html"
+  },
+  {
+    "revision": "10a0727e49ae8e4dc501",
+    "url": "/covid-tracker/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "86bcf8b5fa5ef061d8e7",
+    "url": "/covid-tracker/static/js/2.411939da.chunk.js"
+  },
+  {
+    "revision": "767dfe9ea73b92b6cf4a014603f6a03a",
+    "url": "/covid-tracker/static/js/2.411939da.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "10a0727e49ae8e4dc501",
+    "url": "/covid-tracker/static/js/main.0d5d7b33.chunk.js"
+  },
+  {
+    "revision": "d18efbfbd5f9bcddd6ff",
+    "url": "/covid-tracker/static/js/runtime-main.fe48da39.js"
+  }
+]);
